cc4727d2457dcfb016bcba5623b6f48dd1197b64
Yin <zoe@statementdog.com>
2022-10-18 13:50:41 +0800

    parking model validation test

--- spec/models/parking_spec.rb
+++ spec/models/parking_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Parking, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".validate_end_at_with_amount" do
+    it "is invalid without amount" do
+      parking = Parking.new(parking_type: "guest",
+        start_at: Time.now - 6.hours,
+        end_at: Time.now)
+      expect(parking).not_to be_valid
+    end
+
+    it "is invalid without end_at" do
+      parking = Parking.new(parking_type: "guest",
+        start_at: Time.now - 6.hours,
+        amount: 999)
+      expect(parking).not_to be_valid
+    end
+  end
 end
